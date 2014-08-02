f5f0d98eba0eb4079243d4071a6eb5e7c3a0ace1
Michael Birtwell <michael.birtwell@gmail.com>
2014-08-02 22:58:09 +0100

    Some pretty-fying

--- server/handlers.dart
+++ server/handlers.dart
@@ -4,5 +4,5 @@ import "dart:async";
 import "../lib/urls.dart" as urls;
 
 Future createMindMap(HttpRequest request) {
-  return request.response.redirect(new Uri(path:urls.map.reverse(['1'])));
+  return request.response.redirect(new Uri(path:urls.map.reverse(['1001'])));
 }
