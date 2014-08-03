32f98eab56dab076a3f674f9858f8adc83320707
Michael Birtwell <michael.birtwell@gmail.com>
2014-08-03 20:29:47 +0100

    Simple implementation of new function tests
    Need big refactor now

--- web/mindmap.dart
+++ web/mindmap.dart
@@ -4,4 +4,17 @@ import '/lib/urls.dart' as urls;
 main () {
   querySelector('#idIndicator')
     ..text = urls.map.parse(window.location.pathname)[0];
+  querySelector('.addnode button.add')
+    ..onClick.listen(addNode);
+}
+
+addNode(Event e) {
+  e.preventDefault();
+  Element addNode = (e.target as Element).parent;
+  addNode.remove();
+  querySelector('body').append(
+    new DivElement()
+      ..classes.add('node')
+      ..text = 'herbs'
+  );
 }
