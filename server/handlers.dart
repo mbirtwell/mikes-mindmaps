238b4753afb493a4098d8e8c32edf8f52c30b9ca
Michael Birtwell <michael.birtwell@gmail.com>
2014-08-06 23:27:29 +0100

    Refactor addNode to be more dart-y
    No nested callbacks

--- server/handlers.dart
+++ server/handlers.dart
@@ -17,12 +17,13 @@ Future addNode(HttpRequest req) {
   var args = urls.addToMap.parse(req.uri.path);
   // assume that we only get one event
   return JSON_TO_BYTES.decoder.bind(req).single.then((data) {
-    var contents = data["contents"];
-    return Core.instance.addNode(int.parse(args[0]), contents).then((nodeId) {
-      req.response.write(JSON_TO_BYTES.encode({
-        'id': nodeId
-      }));
-      return req.response.close();
-    });
+    return data["contents"];
+  }).then((contents) {
+    return Core.instance.addNode(int.parse(args[0]), contents);
+  }).then((nodeId) {
+    req.response.write(JSON_TO_BYTES.encode({
+      'id': nodeId
+    }));
+    return req.response.close();
   });
 }
