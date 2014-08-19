fd223c0d4ce604bbcabd621eab2cdf8bd758c218
Michael Birtwell <michael.birtwell@gmail.com>
2014-08-19 23:13:58 +0100

    Dynamic layout/debug hexgrid

--- web/mindmap.dart
+++ web/mindmap.dart
@@ -1,21 +1,60 @@
 import 'dart:html';
 import 'dart:convert';
+import 'dart:math';
+import 'dart:svg' as svg;
 import '/lib/urls.dart' as urls;
 
 int mapId;
+int hexSize = 150;
+
+addHex(x, y) {
+  svg.SvgSvgElement svgEl = querySelector("svg.background");
+  var hex = new svg.PolygonElement();
+  hex.classes.add('wireframe');
+  svgEl.append(hex);
+
+  var r = hexSize;
+  var ox = cos(30 * PI / 180);
+  var cx = window.innerWidth/2 + x * 2 * ox * r + y * 1.5 * r * tan(30 * PI / 180);
+  var cy = window.innerHeight/2 + y * 1.5 * r;
+  hex.points
+    ..appendItem(svgEl.createSvgPoint()..x = cx ..y = cy - r)
+    ..appendItem(svgEl.createSvgPoint()..x = cx + r * ox ..y = cy - r/2)
+    ..appendItem(svgEl.createSvgPoint()..x = cx + r * ox ..y = cy + r/2)
+    ..appendItem(svgEl.createSvgPoint()..x = cx ..y = cy + r)
+    ..appendItem(svgEl.createSvgPoint()..x = cx - r * ox ..y =  cy + r/2)
+    ..appendItem(svgEl.createSvgPoint()..x = cx - r * ox ..y =  cy - r/2)
+  ;
+
+  svgEl.append(new svg.TextElement()
+    ..setAttribute('x', "$cx")
+    ..setAttribute('y', cy.toString())
+    ..text = "$x, $y"
+  );
+}
 
 main () {
   mapId = int.parse(urls.map.parse(window.location.pathname)[0]);
   querySelector('#idIndicator').text = mapId.toString();
+
+  var addNodeForm = querySelector('.addnode');
+  addNodeForm
+    ..style.top = "${window.innerHeight/2 - addNodeForm.offsetHeight/2}px"
+    ..style.left = "${window.innerWidth/2 - addNodeForm.offsetWidth/2}px"
+  ;
   querySelector('.addnode button.add')
     ..onClick.listen(addNode);
+
+  for(var x = -2; x < 3; ++x) {
+    for(var y = -1; y < 2; ++y) {
+      addHex(x, y);
+    }
+  }
 }
 
 makeNode(text) {
   var div = new DivElement()
     ..classes.add('node')
-    ..style.top = "${window.innerHeight/2 - 30}px"
-    ..style.left = "${window.innerWidth/2 - 100}px"
   ;
   for(var pos in ["top", "left", "right", "bottom"]) {
     div.append(new ButtonElement()
@@ -24,6 +63,11 @@ makeNode(text) {
     );
   }
   div.append(new SpanElement()..text = text);
+  querySelector('body').append(div);
+  div
+    ..style.top = "${window.innerHeight/2 - div.offsetHeight/2}px"
+    ..style.left = "${window.innerWidth/2 - div.offsetWidth/2}px"
+  ;
   return div;
 }
 
@@ -48,6 +92,6 @@ addNode(Event e) {
       return;
     }
     addNode.remove();
-    querySelector('body').append(makeNode(nodeText));
+    makeNode(nodeText);
   });
 }
