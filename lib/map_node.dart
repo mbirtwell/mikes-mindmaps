6f8293736be3364ab4756386e40ab1ccc7376998
Michael Birtwell <michael.birtwell@gmail.com>
2014-09-19 23:21:54 +0100

    Add a mind map node class

---
+++ lib/map_node.dart
@@ -0,0 +1,61 @@
+import "dart:math";
+import "dart:convert";
+
+class MindMapNode {
+  String contents;
+  Point position;
+  Point parent;
+
+  MindMapNode(this.contents, this.position, this.parent) {
+    if(this.position == null) {
+      throw new ArgumentError("Position must be set");
+    }
+    if(this.parent == null && this.position != new Point(0, 0)) {
+      throw new ArgumentError("All non central points must have a parent");
+    }
+  }
+
+  MindMapNode.fromMap(Map data):
+    this(data['contents'],
+         _pointFromMap(data['position']),
+         _pointFromMap(data['parent']));
+
+  MindMapNode.fromJson(String json): this.fromMap(JSON.decode(json));
+
+  static Map _pointToMap(Point p) {
+    if(p == null) {
+      return null;
+    }
+    return {
+      'x': p.x,
+      'y': p.y,
+    };
+  }
+
+  static _pointFromMap(Map m) {
+    if(m == null) {
+      return null;
+    }
+    return new Point(m['x'], m['y']);
+  }
+
+  toJson() {
+    var data = {
+        'contents': this.contents,
+        'position': _pointToMap(this.position),
+    };
+    if(parent == null) {
+      data['parent'] = null;
+    } else {
+      data['parent'] = _pointToMap(this.parent);
+    }
+    return JSON.encode(data);
+  }
+
+  bool operator ==(MindMapNode other) {
+    return this.contents == other.contents &&
+      this.position == other.position &&
+      this.parent == other.parent;
+  }
+
+}
---
+++ test/test_map_node.dart
@@ -0,0 +1,64 @@
+import "dart:convert";
+import "dart:math";
+import "package:unittest/unittest.dart";
+
+import "../lib/map_node.dart";
+
+main () {
+  test('constructed central map node has expected attributes', () {
+    var node = new MindMapNode("contents", new Point(0, 0), null);
+    expect(node.contents, equals("contents"));
+    expect(node.position, equals(new Point(0, 0)));
+    expect(node.parent, isNull);
+  });
+  test('constructed map node has expected attributes', () {
+    var node = new MindMapNode("contents", new Point(1, 0), new Point(0, 0));
+    expect(node.contents, equals("contents"));
+    expect(node.position, equals(new Point(1, 0)));
+    expect(node.parent, equals(new Point(0, 0)));
+  });
+  test('identically contstructed nodes compare equal', () {
+    var node1 = new MindMapNode("contents", new Point(1, 0), new Point(0, 0));
+    var node2 = new MindMapNode("contents", new Point(1, 0), new Point(0, 0));
+    expect(node1 == node2, isTrue);
+  });
+  test("nodes with different content don't compare equal", () {
+    var node1 = new MindMapNode("contents 1", new Point(1, 0), new Point(0, 0));
+    var node2 = new MindMapNode("contents", new Point(1, 0), new Point(0, 0));
+    expect(node1 == node2, isFalse);
+  });
+  test("nodes with different positions don't compare equal", () {
+    var node1 = new MindMapNode("contents", new Point(1, 0), new Point(0, 0));
+    var node2 = new MindMapNode("contents", new Point(1, -1), new Point(0, 0));
+    expect(node1 == node2, isFalse);
+  });
+  test("nodes with different parents don't compare equal", () {
+    var node1 = new MindMapNode("contents", new Point(1, 0), new Point(0, 0));
+    var node2 = new MindMapNode("contents", new Point(1, 0), new Point(1, 1));
+    expect(node1 == node2, isFalse);
+  });
+  test("from json constructor loads the data", () {
+    var node = new MindMapNode.fromJson('{"contents": "some contents", "position": {"x": 1, "y": 0}, "parent": {"x": 0, "y": 0}}');
+    expect(node, equals(new MindMapNode("some contents", new Point(1, 0), new Point(0, 0))));
+  });
+  test("from json constructor loads the data with null parent", () {
+    var node = new MindMapNode.fromJson('{"contents": "some contents", "position": {"x": 0, "y": 0}, "parent": null}');
+    expect(node, equals(new MindMapNode("some contents", new Point(0, 0), null)));
+  });
+  test("to json contains the expected data", () {
+    var node = new MindMapNode("contents", new Point(1, 0), new Point(0, 0));
+    expect(JSON.decode(node.toJson()), equals({
+      'contents': 'contents',
+      'position': {'x': 1, 'y': 0},
+      'parent': {'x': 0, 'y': 0},
+    }));
+  });
+  test("to json contains the expected data with null parent", () {
+    var node = new MindMapNode("contents", new Point(0, 0), null);
+    expect(JSON.decode(node.toJson()), equals({
+        'contents': 'contents',
+        'position': {'x': 0, 'y': 0},
+        'parent': null,
+    }));
+  });
+}
